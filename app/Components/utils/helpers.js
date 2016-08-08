5345d8037661650ca2acfff6b7dffea42a8402de
John Dispennette <jtdartworks@gmail.com>
2016-08-07 20:21:23 -0400

    Been working for about 6 hours today going over old code and notes from class along with videos on youtube I think I have a nice 70 percent finished app here.... just going to upload what I have so far. I believe I have done the json package correctly and all the npm installs I need plus a core of code that I can go back thru and tweak after dinner and the rest of week till Wed.

---
+++ app/Components/utils/helpers.js
@@ -0,0 +1,53 @@
+// Include the axios package for performing HTTP requests (promise based alternative to request)
+var axios = require('axios');
+
+// NYT API
+var nytAPI = "259791db35264f3eb2142b039541a24a";
+
+// Helper Functions (in this case the only one is runQuery)
+var helpers = {
+
+	// This function serves our purpose of running the query to nyt. 
+	runQuery: function(location){
+
+		console.log(location);
+
+		//Figure out the nyt
+		var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?query=" + location + "&pretty=1&key=" + nytAPI;
+
+		return axios.get(queryURL)
+			.then(function(response){
+
+				console.log(response);
+				return response.data.results[0].formatted;
+		})
+
+	},
+
+	// This function hits our own server to retrieve the record of query results
+	getHistory: function(){
+
+		return axios.get('/api')
+			.then(function(response){
+
+				console.log(response);
+				return response;
+			});
+	},
+
+	// This function posts new searches to our database.
+	postHistory: function(location){
+
+		return axios.post('/api', {location: location})
+			.then(function(results){
+
+				console.log("Posted to MongoDB");
+				return(results);
+			})
+	}
+
+}
+
+
+// We export the helpers function 
+module.exports = helpers;
